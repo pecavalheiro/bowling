09317f1a2d62031272099e680379bbde813a8e8f
Pedro Cavalheiro <pedrohc@gmail.com>
2019-12-20 20:54:41 +0100

    Extract common methods from ThrowService

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,7 +1,39 @@
 # frozen_string_literal: true
 
 class ApplicationService
+  attr_reader :game, :knocked_pins
+
   def self.call(*args, &block)
     new(*args, &block).call
+  end
+
+  def initialize(game, knocked_pins)
+    @game = game
+    @knocked_pins = knocked_pins
+    set_internals
+  end
+
+  def strike?
+    current_frame.ball_1 == 10
+  end
+
+  def spare?
+    current_frame.ball_1.to_i < 10 &&
+      (current_frame.ball_1.to_i + current_frame.ball_2.to_i) == 10
+  end
+
+  def set_internals
+    current_frame
+    current_player
+  end
+
+  def current_frame
+    @current_frame ||= @game.frames
+                            .find_by(player_id: current_player,
+                                     number: @game.current_frame)
+  end
+
+  def current_player
+    @current_player ||= @game.current_player
   end
 end
--- app/services/throw_processor.rb
+++ app/services/throw_processor.rb
@@ -3,12 +3,6 @@
 class ThrowProcessor < ApplicationService
   attr_reader :game, :knocked_pins
 
-  def initialize(game, knocked_pins)
-    @game = game
-    @knocked_pins = knocked_pins
-    set_internals
-  end
-
   def call
     raise GameHasEndedError if game_has_ended?
 
@@ -68,20 +62,6 @@ class ThrowProcessor < ApplicationService
     end
   end
 
-  def strike?
-    current_frame.ball_1 == 10
-  end
-
-  def spare?
-    current_frame.ball_1.to_i < 10 &&
-      (current_frame.ball_1.to_i + current_frame.ball_2.to_i) == 10
-  end
-
-  def set_internals
-    current_frame
-    current_player
-  end
-
   def change_frame
     @game.current_frame += 1
     @game.save!
@@ -90,15 +70,5 @@ class ThrowProcessor < ApplicationService
   def change_player
     @game.current_player = (@game.current_player == 1 ? 2 : 1)
     @game.save!
-  end
-
-  def current_frame
-    @current_frame ||= @game.frames
-                            .find_by(player_id: current_player,
-                                     number: @game.current_frame)
-  end
-
-  def current_player
-    @current_player ||= @game.current_player
   end
 end
